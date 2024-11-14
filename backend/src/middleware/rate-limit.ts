2df67c50e7fad48d2bb763523f9be8093c83b03e
Luca <61409445+melusc@users.noreply.github.com>
2024-11-14 10:11:32 +0100

    Fix rate-limits per-type instead of per-route

--- backend/src/middleware/rate-limit.ts
+++ backend/src/middleware/rate-limit.ts
@@ -1,4 +1,4 @@
-import rateLimit from 'express-rate-limit';
+import rateLimit, { type RateLimitRequestHandler } from 'express-rate-limit';
 
 // Separate rate limits per kind of request
 // One for GET where there aren't any database reads
@@ -7,29 +7,35 @@ import rateLimit from 'express-rate-limit';
 // These use different stores for rate limits
 // so visiting / doesn't affect the rate limit for accessing a file
 
+let getStatic: RateLimitRequestHandler | undefined; 
 export function rateLimitGetStatic() {
-	return rateLimit({
+	getStatic ??= rateLimit({
 		windowMs: 5 * 60 * 1000, // 5 minutes
-		limit: 200, // Limit each IP to 100 requests per window
+		limit: 100, // Limit each IP to 100 requests per window
 		standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 		legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 	});
+	return getStatic;
 }
 
+let getDatabase: RateLimitRequestHandler | undefined
 export function rateLimitGetDatabase() {
-	return rateLimit({
-		windowMs: 15 * 60 * 1000,
-		limit: 50,
+	getDatabase ??= rateLimit({
+		windowMs: 5 * 60 * 1000,
+		limit: 25,
 		standardHeaders: true,
 		legacyHeaders: false,
 	});
+	return getDatabase;
 }
 
+let post: RateLimitRequestHandler | undefined
 export function rateLimitPost() {
-	return rateLimit({
+	post ??= rateLimit({
 		windowMs: 15 * 60 * 1000,
 		limit: 20,
 		standardHeaders: true,
 		legacyHeaders: false,
 	});
+	return post;
 }
